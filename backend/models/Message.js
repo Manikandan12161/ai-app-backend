2ad77d6f04750b70678b2729219226b77c0da7bc
Manikandan12161 <manikandanswetha0124@gmail.com>
2025-04-14 22:43:20 +0530

    Add files via upload

---
+++ backend/models/Message.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+  prompt: String,
+  response: String,
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Message', messageSchema);
